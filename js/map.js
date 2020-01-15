945273d44a23fc3e0e551c542d4f5a7cdb579a5b
Elsa Schieffelin <ems18@rice.edu>
2020-01-14 21:31:54 -0600

    added cia data

--- js/map.js
+++ js/map.js
@@ -7,20 +7,16 @@ var svg = d3.select('body')
             .append('g');
 var proj =  d3.geoMercator()
                 .scale(130)
-                .translate([width/2, height/3]);
+                .translate([width/2, height/2]);
 var path = d3.geoPath().projection(proj);
 d3.json('js/world_countries.json', function(data) {
     var features = data.features;
-    for (var i = 0; i <features.length; i ++) {
-        var countryName = features[i]['properties']['name'];
-        var countryCoords = features[i]['geometry']['coordinates'];
-        svg.append('g')
-            .attr('class', 'country')
-            .selectAll('path')
-            .data(features)
-            .enter().append('path')
-            .attr('d', path)
-            .style('fill', 'white')
-            .style('stroke', 'black')
-    }
+    svg.append('g')
+        .attr('class', 'country')
+        .selectAll('path')
+        .data(features)
+        .enter().append('path')
+        .attr('d', path)
+        .style('fill', 'white')
+        .style('stroke', 'black')
 });
