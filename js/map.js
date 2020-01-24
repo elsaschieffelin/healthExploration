6864b48df1a40d8d71edba3ac6daf391cbba2bbf
Elsa Schieffelin <ems18@rice.edu>
2020-01-24 08:01:41 -0600

    I added stuff

--- js/map.js
+++ js/map.js
@@ -11,6 +11,21 @@ var proj =  d3.geoMercator()
 var path = d3.geoPath().projection(proj);
 d3.json('js/world_countries.json', function(data) {
     var features = data.features;
+    var countryArray = [];
+    features.forEach(function(data) {
+        countryArray.push(data['properties']['name']);
+  
+    d3.json('data/cia_data.json', function(cia) {
+        var entry= Object.values(cia);
+        entry.forEach(function(d){
+            var country = d['country'];
+            var obseityRate = d['obseity_prevalence_rate'];
+            if (countryArray.includes(d)) {
+                data['obesityRate'].push(obseityRate);
+            } 
+        })
+    })
+})
     svg.append('g')
         .attr('class', 'country')
         .selectAll('path')
@@ -19,4 +34,6 @@ d3.json('js/world_countries.json', function(data) {
         .attr('d', path)
         .style('fill', 'white')
         .style('stroke', 'black')
+   console.log(features);
 });
+
